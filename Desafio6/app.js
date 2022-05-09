61321cbcee90cc2c79e309d04bd8f5891eea9542
Maty Fusari <matyfusari02@hotmail.com>
2022-05-09 13:10:48 -0300

    Desafio6

---
+++ Desafio6/app.js
@@ -0,0 +1,46 @@
+const express = require("express");
+
+var handlebars = require("express-handlebars").create({
+  defaultLayout: "main",
+});
+const PORT = process.env.PORT || 8080
+const routes = require("./api/routes");
+const path = "./text.json";
+const Container = require("./utils/classes/Container");
+const container = new Container(path);
+const app = express();
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
+
+app.use(express.static("public"));
+app.use("/static", express.static(__dirname + "/public"));
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.engine("handlebars", handlebars.engine);
+app.set("view engine", "handlebars");
+
+app.get("/", (req, res) => {
+  res.render("add.hbs");
+});
+
+io.on("connection", (socket) => {
+  socket.on("add product", (msg) => {
+    io.emit("add product", msg);
+  });
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+  });
+});
+
+app.get("/products", async (req, res) => {
+  res.render("index.hbs", { products: await container.getAllItems() });
+});
+
+app.use("/api", routes);
+
+// Listen on port 8080
+http.listen(PORT, () => {
+  console.log("Server run on port" + PORT );
+});
